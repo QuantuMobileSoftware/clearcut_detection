42bba35e90033ee034cf1e2327530548bcfb3325
Andrew <andrey.nesm57@gmail.com>
2019-05-26 13:40:30 +0300

    Added different models, augmentations
    Random augmentations: rotation, flips, RGBshift, CLAHE, RandomSizedCrop
    Models: Unet with resnet34, resnet50, vgg11 and Linknet

---
+++ scripts/datasets.py
@@ -0,0 +1,67 @@
+import collections
+import os
+
+import numpy as np
+from PIL import Image
+from albumentations import (
+    CLAHE, RandomRotate90, Flip, OneOf, Compose, RandomSizedCrop, RGBShift
+)
+from albumentations.pytorch.transforms import ToTensor
+from catalyst.dl.utils import UtilsFactory
+
+from params import args
+
+
+def get_image(image_info):
+    dataset_path = args.dataset_path
+    # img_path = os.path.join(dataset_path, image_info["dataset_folder"], "images", image_info["image_name"] + ".tiff")
+    # mask_path = os.path.join(dataset_path, image_info["dataset_folder"], "masks", image_info["image_name"] + ".png")
+
+    img_path = os.path.join(dataset_path, "images", image_info["image_name"] + ".tiff")
+    mask_path = os.path.join(dataset_path, "masks", image_info["image_name"] + ".png")
+
+    img = Image.open(img_path)
+    mask = Image.open(mask_path)
+
+    img_array = np.array(img)
+    mask_array = np.array(mask)
+
+    aug = Compose([
+        RandomRotate90(),
+        Flip(),
+        OneOf([
+            RGBShift(),
+            CLAHE(clip_limit=2)
+        ], p=0.4),
+        RandomSizedCrop(min_max_height=(int(args.img_height * 0.7), args.img_height), height=args.img_height,
+                        width=args.img_width),
+        ToTensor()
+    ], p=0.9)
+
+    augmented = aug(image=img_array, mask=mask_array)
+
+    augmented_img = augmented['image']
+    augmented_mask = augmented['mask']
+
+    return {"features": augmented_img, "targets": augmented_mask}
+
+
+def create_loaders(train_df, val_df):
+    train_loader = UtilsFactory.create_loader(
+        train_df,
+        open_fn=get_image,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers,
+        shuffle=True)
+
+    valid_loader = UtilsFactory.create_loader(
+        val_df,
+        open_fn=get_image,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers,
+        shuffle=True)
+
+    loaders = collections.OrderedDict()
+    loaders["train"] = train_loader
+    loaders["valid"] = valid_loader
+    return loaders
--- scripts/losses.py
+++ scripts/losses.py
@@ -1,9 +1,24 @@
-def dice_loss(pred, target, smooth=1.):
-    pred = pred.contiguous()
-    target = target.contiguous()
+import torch.nn as nn
+import torch
+from torch.nn import functional as F
+from catalyst.dl.metrics import dice
 
-    intersection = (pred * target).sum(dim=2).sum(dim=2)
 
-    loss = (1 - ((2. * intersection + smooth) / (pred.sum(dim=2).sum(dim=2) + target.sum(dim=2).sum(dim=2) + smooth)))
+class BCE_Dice_Loss(torch.nn.Module):
 
-    return loss.mean()
+    def __init__(self, bce_weight=0.5):
+        super(BCE_Dice_Loss, self).__init__()
+        self.bce_weight = bce_weight
+
+    def forward(self, x, y):
+        bce = F.binary_cross_entropy_with_logits(x, y)
+
+        dice = dice_loss(x, y)
+
+        loss = bce * self.bce_weight + dice * (1 - self.bce_weight)
+
+        return loss
+
+
+def dice_loss(pred, target, eps=1.):
+    return 1 - dice(pred, target, eps)
