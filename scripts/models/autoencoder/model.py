495052bf989fa2ab067c0c5868f4b90c73e2d36a
Andrew <andrey.nesm57@gmail.com>
2019-06-14 14:10:25 +0300

    Working on adding autoencoder for resnet50 training

---
+++ scripts/models/autoencoder/model.py
@@ -0,0 +1,32 @@
+from segmentation_models_pytorch.base import EncoderDecoder
+from segmentation_models_pytorch.encoders import get_encoder
+from .decoder import UnetDecoder
+
+
+class Autoencoder_Unet(EncoderDecoder):
+
+    def __init__(
+            self,
+            encoder_name='resnet34',
+            encoder_weights='imagenet',
+            decoder_use_batchnorm=True,
+            decoder_channels=(256, 128, 64, 32, 16),
+            classes=1,
+            activation='sigmoid',
+            center=False,  # usefull for VGG models
+    ):
+        encoder = get_encoder(
+            encoder_name
+        )
+
+        decoder = UnetDecoder(
+            encoder_channels=encoder.out_shapes,
+            decoder_channels=decoder_channels,
+            final_channels=classes,
+            use_batchnorm=decoder_use_batchnorm,
+            center=center,
+        )
+
+        super().__init__(encoder, decoder, activation)
+
+        self.name = 'u-{}'.format(encoder_name)
