Channel scaling fixed, updated path to dataset

pytorch/datasets.py
@@ -67,14 +67,25 @@ def read_tensor(filepath):
     return imageio.imread(filepath)
 
 
+def scale(tensor, axis):
+    for i in range(tensor.shape[axis]):
+        max_value = tensor[:, :, i].max()
+        if max_value != 0:
+            tensor[:, :, i] = tensor[:, :, i] / max_value
+
+    return tensor * 255
+
+
 def get_input_pair(
-    data_info_row, channels=args.channels, data_path=args.data_path,
+    data_info_row, channels=args.channels,
+    data_path=args.data_path, input_folder=args.input_folder,
     image_folder=args.images_folder, mask_folder=args.masks_folder,
     image_type=args.image_type, mask_type=args.mask_type
 ):
     if len(channels) == 0:
         raise Exception('You have to specify at least one channel.')
 
+    year = str(data_info_row['date'])[:4]
     dataset = get_fullname(
         data_info_row['date'],
         data_info_row['name']
@@ -84,12 +95,14 @@ def get_input_pair(
         data_info_row['ix'], data_info_row['iy']
     )
     image_path = get_filepath(
-        data_path, dataset, image_folder,
-        filename, file_type=image_type
+        data_path, input_folder, year,
+        dataset, image_folder, filename,
+        file_type=image_type
     )
     mask_path = get_filepath(
-        data_path, dataset, mask_folder,
-        filename, file_type=mask_type
+        data_path, input_folder, year,
+        dataset, mask_folder, filename,
+        file_type=mask_type
     )
 
     image_tensor = filter_by_channels(
@@ -100,7 +113,7 @@ def get_input_pair(
     if image_tensor.ndim == 2:
         image_tensor = image_tensor.reshape(*image_tensor.shape, 1)
 
-    images_array = (image_tensor / image_tensor.max() * 255).astype(np.uint16)
+    images_array = scale(image_tensor, 2).astype(np.uint16)
     masks_array = read_tensor(mask_path)
 
     if channels[0] == 'rgb':