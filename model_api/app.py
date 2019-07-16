682caaf40c185cc644b623c83cb7da8b9399e922
Andrew <andrey.nesm57@gmail.com>
2019-07-16 13:50:26 +0300

    Added support for several models at one endpoint, changed endpoint name.

--- model_api/app.py
+++ model_api/app.py
@@ -13,8 +13,8 @@ from scripts.predict_raster import predict_raster, polygonize, save_polygons
 app = Flask(__name__)
 
 
-@app.route('/detect_clearcuts', methods=['POST'])
-def detect_clearcuts():
+@app.route('/raster_prediction', methods=['POST'])
+def raster_prediction():
     data = request.get_json()
     image_path = data['image_path']
     if len(image_path) == 0:
@@ -23,21 +23,33 @@ def detect_clearcuts():
         filename = re.split(r'[./]', image_path)[-2]
     else:
         abort(400)
-    predicted_filename = f'predicted_{filename}'
-    channels, network, model_weights_path, save_path, threshold, input_size = load_config()
+
+    models, save_path, threshold, input_size = load_config()
     result_directory_path = os.path.join(save_path, filename)
     os.makedirs(result_directory_path, exist_ok=True)
+    path_array = []
 
-    raster_array, meta = predict_raster(
-        image_path, channels,
-        network, model_weights_path, input_size=input_size
-    )
-    save_raster(raster_array, result_directory_path, filename)
+    for model in models:
+        predicted_filename = f'predicted_{model}_{filename}'
 
-    polygons = polygonize(raster_array, meta, threshold)
-    save_polygons(polygons, meta, result_directory_path, predicted_filename)
-    return jsonify({"picture": os.path.join(result_directory_path, predicted_filename + '.png'),
-                    "polygons": os.path.join(result_directory_path, predicted_filename + '.geojson')})
+        channels = models[model]["channels"]
+        network = models[model]["network"]
+        model_weights_path = models[model]["weights"]
+        raster_array, meta = predict_raster(
+            image_path, channels,
+            network, model_weights_path, input_size=input_size
+        )
+        save_raster(raster_array, result_directory_path, filename)
+
+        polygons = polygonize(raster_array, meta, threshold)
+        save_polygons(polygons, meta, result_directory_path, predicted_filename)
+
+        path_array.append({
+            "picture": os.path.join(result_directory_path, predicted_filename + '.png'),
+            "polygons": os.path.join(result_directory_path, predicted_filename + '.geojson')
+        })
+
+    return jsonify(path_array)
 
 
 def save_raster(raster_array, save_path, filename):
@@ -49,13 +61,11 @@ def load_config():
     with open("config.yml", 'r') as config:
         cfg = yaml.load(config, Loader=yaml.SafeLoader)
 
-    network = cfg['model']['network']
-    channels = cfg['model']['channels']
-    model_weights_path = cfg['model']['weights']
+    models = cfg['models']
     save_path = cfg['prediction']['save_path']
     threshold = cfg['prediction']['threshold']
     input_size = cfg['prediction']['input_size']
-    return channels, network, model_weights_path, save_path, threshold, input_size
+    return models, save_path, threshold, input_size
 
 
 if __name__ == '__main__':
