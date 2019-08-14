953d3831499274e58059cbcf98989a7c9cfa5ca6
Ilya Zinckevich <ilya.zinckevich@gmail.com>
2019-08-14 11:22:58 +0300

    9082. Fixed setting of model endpoint request

--- clearcut_detection_backend/model_call.py
+++ clearcut_detection_backend/model_call.py
@@ -1,14 +1,17 @@
 import json
-import os
 import requests
 import yaml
 
 
 def raster_prediction(tif_path):
-    with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'model_call_config.yml'), 'r') as config:
+    with open('model_call_config.yml', 'r') as config:
         cfg = yaml.load(config, Loader=yaml.SafeLoader)
     model_api_cfg = cfg["model-api"]
-    API_ENDPOINT = model_api_cfg["url"] + ':' + str(model_api_cfg["port"]) + model_api_cfg["endpoint"]
+    API_ENDPOINT = "http://{host}:{port}/{endpoint}".format(
+        host=model_api_cfg["host"],
+        port=model_api_cfg["port"],
+        endpoint=model_api_cfg["endpoint"]
+    )
     data = {"image_path": tif_path}
     r = requests.post(url=API_ENDPOINT, json=data)
     result = r.text
