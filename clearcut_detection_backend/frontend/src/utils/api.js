dd49d3bf6dd2cca6925cded625d18379342efb24
yaroslav-perec <perets.yaroslav@gmail.com>
2019-08-21 14:41:21 +0300

    Minor fix

--- clearcut_detection_backend/frontend/src/utils/api.js
+++ clearcut_detection_backend/frontend/src/utils/api.js
@@ -1,8 +1,3 @@
-/*
- * Copyright (C) Cloud Technology Partners - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- */
 
 import { dataToQueryParams } from './util';
 
