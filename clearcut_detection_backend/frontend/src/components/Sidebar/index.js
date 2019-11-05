9c9071a4897ae61d603545df4ab07b328ce1b794
Yaroslav Demenskiy <yaroslav.demenskiy@gmail.com>
2019-11-05 13:09:45 +0200

    Sidebar component is created

---
+++ clearcut_detection_backend/frontend/src/components/Sidebar/index.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+import { ReactComponent as Logo } from '../../assets/images/logo.svg';
+
+import Switch from '../Switch';
+
+import './style.css';
+
+class Sidebar extends Component {
+  state = {
+    isOpened: false
+  };
+
+  handleClick = () => this.setState({ isOpened: !this.state.isOpened });
+
+  render() {
+    const { isOpened } = this.state;
+    const { children } = this.props;
+
+    return (
+      <aside className="sidebar">
+        <header className="sidebar-header">
+          <div className="logo">
+            <Logo />
+          </div>
+          <Switch state={isOpened} handleClick={this.handleClick} />
+        </header>
+        {isOpened && <div className="sidebar-body">{children}</div>}
+      </aside>
+    );
+  }
+}
+
+export default Sidebar;
