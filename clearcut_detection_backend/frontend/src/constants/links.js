7ef82d860fc15e69be49265dd893b393e2e3241e
Yaroslav Demenskiy <yaroslav.demenskiy@gmail.com>
2019-11-06 14:37:34 +0200

    constants with links added

---
+++ clearcut_detection_backend/frontend/src/constants/links.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    url: 'https://github.com/QuantuMobileSoftware/clearcut_detection',
+    text: 'GitHub repository'
+  },
+  {
+    url:
+      'https://github.com/QuantuMobileSoftware/clearcut_detection/blob/master/LICENCE',
+    text: 'Licence'
+  },
+  { url: 'https://scgis.org.ua/', text: 'Partners: GIS' }
+];
