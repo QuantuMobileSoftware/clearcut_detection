2bdd27bb03e97622adeb5f0827073b2cc8275881
Andriy <a.koval@quantumobile.com>
2020-03-05 16:13:03 +0200

    bugfix ### changes - add local fonts

--- clearcut_detection_backend/frontend/src/config/index.js
+++ clearcut_detection_backend/frontend/src/config/index.js
@@ -3,9 +3,9 @@ export const DATE_FORMAT = {
 };
 
 export const CUSTOM_LEGEND_DATA = [
-  { color: 'red', label: 'Clear cut that recently increased in size' },
-  { color: 'yellow', label: 'Clear cut that has not changed in size' },
-  { color: 'grey', label: 'Clear cut with no data available' }
+  { color: 'red', label: 'Clearcut that recently increased in size' },
+  { color: 'yellow', label: 'Clearcut that has not changed in size' },
+  { color: 'grey', label: 'Clearcut with no data available' }
 ];
 
 export const CHART_COLORS = ['#5E95D6', '#55BACF', '#DCA7D3'];
