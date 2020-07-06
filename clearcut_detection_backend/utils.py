262b90ae1d818ac02d2b249958b6c12fe2ac76c3
dyman <dyman@lug.in.ua>
2020-07-06 20:39:26 +0300

    add few common functions for working with files and zip archives to utils

--- clearcut_detection_backend/utils.py
+++ clearcut_detection_backend/utils.py
@@ -4,6 +4,10 @@ Script helpers
 import os
 import warnings
 from enum import Enum
+from zipfile import ZipFile
+from io import BytesIO
+import requests
+from shutil import copyfile
 
 
 def path_exists_or_create(path):
@@ -22,8 +26,63 @@ class Bands(Enum):
 
 
 def get_landcover():
-    landcover_path = './data/landcover'
+    """
+    deprecated, use services.landcover.Landcover
+    """
+    landcover_path = path_exists_or_create('./data/landcover')
     landcover_file = f'{landcover_path}/forest.tiff'
     os.system(f'./download_landcover.sh {landcover_path}')
     if not os.path.isfile(landcover_file):
         warnings.warn(f'{landcover_file} was not found.', UserWarning)
+
+
+def download_without_progress(url):
+    """
+    Download data from a URL, returning a BytesIO containing the loaded data.
+
+    Parameters
+    ----------
+    url : str
+        A URL that can be understood by ``requests.get``.
+
+    Returns
+    -------
+    data : BytesIO
+        A BytesIO containing the downloaded data.
+    """
+    resp = requests.get(url)
+    resp.raise_for_status()
+    return BytesIO(resp.content)
+
+
+def fetch_all_from_zip(file, path_to_extract):
+    """
+    fetch all files from archive
+    :param file: BytesIO or file path
+    :param path_to_extract:
+    :return:
+    """
+    with ZipFile(file) as zip_file:
+        zip_file.extractall(path_to_extract)
+
+
+def fetch_file_from_zip(file, source, destination):
+    """
+    fetch specific file from archive to destination path
+    :param file: BytesIO or file path
+    :param source: file name to be extracted
+    :param destination: path for extraction
+    :return:
+    """
+    with ZipFile(file) as zip_file:
+        zip_file.extract(source, destination)
+
+
+def copy_file(source, destination):
+    """
+    copy file from source to destination
+    :param source: path to source file
+    :param destination: path to destination file
+    :return:
+    """
+    copyfile(source, destination)
