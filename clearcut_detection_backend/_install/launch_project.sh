fc947b5d7bce020e91975624df1de98990db1faf
dyman <dyman@lug.in.ua>
2020-07-08 18:08:41 +0300

    rm _install

--- clearcut_detection_backend/_install/launch_project.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-cd ..
-docker-compose -f ./docker-compose-for-django-dev.yml up
--- clearcut_detection_backend/_install/run_migrations.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cd ..
-docker-compose -f ./docker-compose-for-django-dev.yml run django python /code/manage.py migrate --noinput
-# TODO find how to remove container after job done.
-#docker-compose -f ./docker-compose-for-django-dev.yml stop django && docker-compose rm -f ./docker-compose-for-django-dev.yml django
