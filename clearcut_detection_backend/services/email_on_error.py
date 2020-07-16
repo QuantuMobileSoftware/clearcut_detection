cea1afb28bb4cf58559c25463504dcdaf53851e5
dyman <dyman@lug.in.ua>
2020-07-16 18:53:51 +0300

    add class EmailOnErrorHandler, and remove test usles function

--- clearcut_detection_backend/services/email_on_error.py
+++ clearcut_detection_backend/services/email_on_error.py
@@ -1,12 +1,66 @@
+import logging
+from copy import copy
 import traceback
 from django.conf import settings
-from django.core.mail import EmailMessage
+from django.core.mail import send_mail, get_connection
+
+from django.views.debug import ExceptionReporter
 
 
-def emaile_on_service_error(subject, error):
-    EmailMessage(
-        subject=subject,
-        body=f'\n\n{str(error)}\n\n {"".join(traceback.format_tb(error.__traceback__))}',
-        from_email=settings.EMAIL_HOST_USER,
-        to=settings.EMAIL_ADMIN_MAILS
-    ).send()
+class EmailOnErrorHandler(logging.Handler):
+    """An exception log handler that emails log entries to site admins.
+
+    If the request is passed as the first argument to the log record,
+    request data will be provided in the email report.
+    """
+
+    def __init__(self, include_html=True, email_backend=None):
+        super().__init__()
+        self.include_html = include_html
+        self.email_backend = email_backend
+
+    def emit(self, record):
+        try:
+            request = record.request
+            subject = '%s (%s IP): %s' % (
+                record.levelname,
+                ('internal' if request.META.get('REMOTE_ADDR') in settings.INTERNAL_IPS
+                 else 'EXTERNAL'),
+                record.getMessage()
+            )
+        except Exception:
+            subject = '%s: %s' % (
+                record.levelname,
+                record.getMessage()
+            )
+            request = None
+        subject = self.format_subject(subject)
+
+        # Since we add a nicely formatted traceback on our own, create a copy
+        # of the log record without the exception data.
+        no_exc_record = copy(record)
+        no_exc_record.exc_info = None
+        no_exc_record.exc_text = None
+
+        if record.exc_info:
+            exc_info = record.exc_info
+        else:
+            exc_info = (None, record.getMessage(), None)
+
+        reporter = ExceptionReporter(request, is_email=True, *exc_info)
+        message = "%s\n\n%s" % (self.format(no_exc_record), reporter.get_traceback_text())
+        html_message = reporter.get_traceback_html() if self.include_html else None
+        send_mail(subject,
+                  message,
+                  from_email=settings.EMAIL_HOST_USER,
+                  recipient_list=settings.EMAIL_ADMIN_MAIL,
+                  fail_silently=False,
+                  html_message=html_message
+                  )
+
+    @staticmethod
+    def format_subject(subject):
+        """
+        Escape CR and LF characters.
+        """
+        return subject.replace('\n', '\\n').replace('\r', '\\r')
