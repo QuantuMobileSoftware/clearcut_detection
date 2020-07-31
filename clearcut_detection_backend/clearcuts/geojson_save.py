cfc4d86469bb170d1dd4f0a00118bdc8a2babebc
dyman <dyman@lug.in.ua>
2020-07-31 12:43:44 +0300

    update clearcut model, add migration

--- clearcut_detection_backend/clearcuts/geojson_save.py
+++ clearcut_detection_backend/clearcuts/geojson_save.py
@@ -41,49 +41,6 @@ def save_clearcut(poly, avg_area, detection_date, forest, cloud, area_in_meters,
         logger.info(f'clearcut saved with id={clearcut.id}')
 
 
-def save(tile, poly_path, init_db=False):
-    predicted_clearcuts = gp.read_file(poly_path)
-    area_geodataframe = predicted_clearcuts['geometry'].area
-    predicted_polys = predicted_clearcuts['geometry'].buffer(0).to_crs({'init': 'epsg:4326'})
-
-    geodataframe = gp.GeoDataFrame(geometry=predicted_polys)
-    flags_forest = predicted_clearcuts['forest'].to_list()
-    flags_clouds = predicted_clearcuts['clouds'].to_list()
-    geospolygons = convert_geodataframe_to_geospolygons(geodataframe)
-    
-    avg_area = np.mean(area_geodataframe)
-    detection_date = [tile.first().tile_date, tile.last().tile_date]
-
-    if Clearcut.objects.all().count() == 0:
-        for idx, geopoly in enumerate(geospolygons):
-            save_clearcut(geopoly, avg_area, detection_date,
-                          flags_forest[idx], flags_clouds[idx],
-                          area_geodataframe[idx], create_new_zone=True)
-    else:
-        for idx, geopoly in enumerate(geospolygons):
-            intersecting_polys = Clearcut.objects.filter(mpoly__distance_lt=(geopoly, D(m=SEARCH_WINDOW)))
-            forest = flags_forest[idx]
-            cloud = flags_clouds[idx]
-            if intersecting_polys.count() > 0:
-                polys = [poly for poly in intersecting_polys]
-                areas = [poly.mpoly.intersection(geopoly).area for poly in polys]
-                dates = [poly.image_date_previous for poly in polys]
-                max_intersection_area = np.argmax(areas)
-                # Union of intersecting forest polygons:
-                detection_date_union = detection_date
-                if flags_forest[idx] == 1 and flags_clouds[idx] == 0:
-                    for poly in polys:
-                        geopoly = geopoly.union(poly.mpoly)
-                    detection_date_union = [tile.first().tile_date, min(dates)]
-                save_clearcut(geopoly, avg_area, detection_date_union,
-                              flags_forest[idx], flags_clouds[idx],
-                              area_geodataframe[idx], zone=polys[max_intersection_area].zone)
-            else:
-                save_clearcut(geopoly, avg_area, detection_date,
-                              flags_forest[idx], flags_clouds[idx],
-                              area_geodataframe[idx], create_new_zone=True)
-
-
 def save_from_task(task_id):
     logger.info(f'task_id: {task_id}')
     start_time = time.time()
--- clearcut_detection_backend/clearcuts/models.py
+++ clearcut_detection_backend/clearcuts/models.py
@@ -2,17 +2,19 @@ from django.contrib.gis.db import models
 from django.utils import timezone
 
 
-class Zone(models.Model):
-    def __str__(self):
-        return f"Zone {self.id}"
-
-
 class Tile(models.Model):
     tile_index = models.CharField(max_length=7, blank=False, null=False)
     is_tracked = models.SmallIntegerField(default=0, null=False)
 
     def __str__(self):
         return self.tile_index
+
+
+class Zone(models.Model):
+    tile_index = models.ForeignKey(Tile, null=True, on_delete=models.CASCADE, related_name='tile_zones')
+
+    def __str__(self):
+        return f"Zone {self.id}"
 
 
 class Clearcut(models.Model):
@@ -64,8 +66,8 @@ class RunUpdateTask(models.Model):
     path_type = models.SmallIntegerField(default=0, null=False)
     path_img_0 = models.URLField(max_length=200, blank=False, null=False, default='')
     path_img_1 = models.URLField(max_length=200, blank=False, null=False, default='')
-    image_date_0 = models.DateField(blank=False, null=False)  # TODO
-    image_date_1 = models.DateField(blank=False, null=False)  # TODO
+    image_date_0 = models.DateField(blank=False, null=False)
+    image_date_1 = models.DateField(blank=False, null=False)
     path_clouds_0 = models.URLField(max_length=200, blank=False, null=False, default='')
     path_clouds_1 = models.URLField(max_length=200, blank=False, null=False, default='')
     result = models.URLField(max_length=200, null=True)
