d9dc7f2f9f7226642778f48e5e397a335046b24b
DmitriyLugovoy <dmitriy.lugovoy.79@gmail.com>
2020-08-27 10:50:12 +0300

    update frontend config.url

--- clearcut_detection_backend/frontend/src/config/url.js
+++ clearcut_detection_backend/frontend/src/config/url.js
@@ -3,9 +3,9 @@ export const URL_PREFIX = '/api';
 
 export const URL = {
   map: {
-    get: (startDate, endDate) => `${URL_PREFIX}/clearcuts_info/${startDate}/${endDate}`,
+    get: (startDate, endDate) => `${URL_PREFIX}/clearcuts/info/${startDate}/${endDate}`,
     polygon: {
-      get: (id, startDate, endDate) => `${URL_PREFIX}/clearcut_area_chart/${id}/${startDate}/${endDate}`
+      get: (id, startDate, endDate) => `${URL_PREFIX}/clearcuts/area_chart/${id}/${startDate}/${endDate}`
     }
   }
 };
