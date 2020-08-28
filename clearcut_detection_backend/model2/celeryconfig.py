df1800e36a12187e2c0bf212fe01a85d4207809c
DmitriyLugovoy <dmitriy.lugovoy.79@gmail.com>
2020-08-28 12:14:18 +0300

    add celery settings

--- clearcut_detection_backend/model2/celeryconfig.py
+++ clearcut_detection_backend/model2/celeryconfig.py
@@ -4,16 +4,18 @@ from config import (POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, DB_HOST,
 task_serializer = 'json'
 result_serializer = 'json'
 accept_content = ['json']
-# timezone = 'Europe/Oslo'
-# enable_utc = True
 
 broker_url = f'amqp://{RABBITMQ_USER}:{RABBITMQ_PASS}@{RABBITMQ_HOST}:{RABBITMQ_PORT_NUMBER}//'
 result_backend = f'db+postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{DB_HOST}/{POSTGRES_DB}'
 
-# result_backend = 'rpc://'
-
-
+worker_prefetch_multiplier = 1
+worker_max_tasks_per_child = 1
+task_acks_late = True
+task_time_limit = 60 * 15
 # database_table_names = {
 #     'task': 'django_celery_results_taskresult',
 #     'group': 'myapp_groupmeta',
 # }
+
+
+
