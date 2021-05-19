c86337ab5f9ed76b5e627311c1f15f6a7c31f99e
DmitriyLugovoy <dmitriy.lugovoy.79@gmail.com>
2021-05-19 09:21:19 +0300

    15374 add path vars to config file

--- clearcut_detection_backend/model2/config/__init__.py
+++ clearcut_detection_backend/model2/config/__init__.py
@@ -1,5 +1,6 @@
 import os
 import yaml
+from pathlib import Path
 
 with open('/model/predict_config.yml', 'r') as config:
     cfg = yaml.load(config, Loader=yaml.SafeLoader)
@@ -18,3 +19,16 @@ RABBITMQ_USER = os.environ.get('RABBITMQ_DEFAULT_USER', 'guest')
 RABBITMQ_PASS = os.environ.get('RABBITMQ_DEFAULT_PASS', 'guest')
 RABBITMQ_HOST = os.environ.get('RABBITMQ_HOST', 'rabbitmq_prod')
 RABBITMQ_PORT_NUMBER = os.environ.get('RABBITMQ_NODE_PORT_NUMBER', 5672)
+CUDA_VISIBLE_DEVICES = os.environ.get('CUDA_VISIBLE_DEVICES', '0')
+
+SCOPES = ['https://www.googleapis.com/auth/drive.file']
+
+LANDCOVER_POLYGONS_PATH = Path('/data/landcover')
+PREDICTED_PATH = Path('/data/predicted')
+LANDCOVER_FILENAME = 'S2A_OPER_GIP_TILPAR_MPC__20151209T095117_V20150622T000000_21000101T000000_B00.kml'
+SENTINEL_TILES = LANDCOVER_POLYGONS_PATH / LANDCOVER_FILENAME
+LANDCOVER_GEOJSON = LANDCOVER_POLYGONS_PATH / 'landcover_polygons.geojson'
+
+CLOUDS_PROBABILITY_THRESHOLD = 15
+NEAREST_POLYGONS_NUMBER = 10
+DATES_FOR_TILE = 2
